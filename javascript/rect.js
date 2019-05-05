375548f6490e5506858e413c08497b7e6eddfbdc
Joseph Saunders <Jsaunderswhs@gmail.com>
2019-05-05 16:10:43 -0400

    gotta nice bar goin

---
+++ javascript/rect.js
@@ -0,0 +1,6 @@
+// var rectangle = document.querySelector('.pop-bar');
+// var ctx = rectangle.getContext("2d");
+// ctx.strokeStyle = 'red';
+// ctx.fillStyle = 'rgb(0,0,255)';
+// ctx.fillRect(10,10,20,100);
+// ctx.stroke();
