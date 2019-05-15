a8fd2cde24311941d02ef4e40ac50ed66fac14ca
Joseph Saunders <Jsaunderswhs@gmail.com>
2019-05-15 17:33:52 -0400

    need to fix the grid i don't get it

--- javascript/picks.js
+++ javascript/picks.js
@@ -9,15 +9,30 @@ $(function() {
 			loadingScreen: $('div.loading-screen'),
 			loaderButton: $('button.btn-load-page'),
 
-			//set default values for page
-			init: function(){
+			initGallery: function(img, imgSrc){
+					let dynamicImg = new Image();
+					dynamicImg.onload = function() {
+						img.src = this.src;
+					};
+					dynamicImg.src = imgSrc;
+			},
+			//init gallery images as dynamic
+			imageInit: function(img){
+				//init images
+
+				let dynamicImg = new Image();
+				dynamicImg.onload = function() {
+					img.src = this.src;
+				};
+				dynamicImg.src = "/images/Divine_Feminine.png";
+				
 			},
 			loadMainPage: function(){
-				mySite.mainBackground.removeClass('hide-ontop');
+				mySite.mainBackground.removeClass('hidden');
 				mySite.loadingScreen.find('.img-chaedles-picks').animate({opacity: '0.0'}, "500");
 				mySite.loadingScreen.find('.overlay').animate({opacity: '0'}, 
-					{
-						duration: 500, 
+				{
+					duration: 500, 
 						// on complete, display main page
 						complete: function(){
 							mySite.mainBackground.css("opacity", "0");
@@ -32,14 +47,14 @@ $(function() {
 							);
 						}
 					}
-				);
+					);
 				mySite.loaderButton.css("border", "none").animate(
-					{
-						opacity: '0'
-					},
-					{
-						duration: 500
-					});
+				{
+					opacity: '0'
+				},
+				{
+					duration: 500
+				});
 			},
 
 			waitLoadButton: function() {
@@ -50,26 +65,30 @@ $(function() {
 	}();
 
 
-function animationDone(){
+	function animationDone(){
 		mySite.loadingScreen.addClass("hidden");
 		alert(mySite.loadingScreen);
-}
+	}
 
 
 
 
-/***Load Starter Page***/
-// mySite.init();
+	/***Load Starter Page***/
 
-mySite.loaderButton.animate({opacity: '1'}, 350);
-mySite.loaderButton.on('keyup', function(event){
-	alert('key');
-});
-mySite.loaderButton.on('click', mySite.loadMainPage);
-/***End Starter Page***/	
+	mySite.loaderButton.animate({opacity: '1'}, 350);
+	mySite.loaderButton.on('click', mySite.loadMainPage);
+	/***End Starter Page***/	
 
-/***Start Picks page***/
+	/***Start Picks page***/
+	mySite.initGallery(document.getElementById("SOTW"), "/images/reptillia.jpg");
+	// load gallery
+	let gallery = Array.from(document.images).slice(1,9);
+	for (let img of gallery) {
+		mySite.initGallery(img, "/images/Divine_Feminine.png");
+	}//end load gallery
 
+
+// mySite.imageInit(document.images[1]);
 /***End Picks page***/
 
 
